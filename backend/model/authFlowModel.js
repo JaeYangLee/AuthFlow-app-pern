5754da2e8401d60701455261a142ad117b87e6d9
Jian Lee <ramosjianlee@gmail.com>
2025-10-25 19:42:18 +0800

    feat: built the RESTful API methods for the model

--- backend/model/authFlowModel.js
+++ backend/model/authFlowModel.js
@@ -25,23 +25,25 @@ const findUserByEmail = async (email) => {
 
 // for profile view
 const findUserByUsername = async (username) => {
-  const result = await pool.query("SELECT * FROM users WHERE username = $1", [
-    username,
-  ]);
+  const result = await pool.query(
+    "SELECT user_id, username, first_name, last_name, location, email, role, created_at FROM users WHERE username = $1",
+    [username]
+  );
   return result.rows[0];
 };
 
 const updateUser = async (user_id, first_name, last_name, location) => {
   const result = await pool.query(
     "UPDATE users SET first_name = $1, last_name = $2, location = $3 WHERE user_id = $4 RETURNING *",
-    [user_id, first_name, last_name, location]
+    [first_name, last_name, location, user_id]
   );
   return result.rows[0];
 };
 
 const deleteUser = async (user_id) => {
   const result = await pool.query(
-    "DELETE from users WHERE user_id = $1 RETURNING *"
+    "DELETE FROM users WHERE user_id = $1 RETURNING *",
+    [user_id]
   );
 
   return result.rows[0];
