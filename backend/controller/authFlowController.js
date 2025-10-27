ab5139c6050ce5f2b965d7d84c9184f54d41ac46
Jian Lee <ramosjianlee@gmail.com>
2025-10-27 17:41:38 +0800

    feat: done building to controller

--- backend/controller/authFlowController.js
+++ backend/controller/authFlowController.js
@@ -39,6 +39,124 @@ const createUser = async (req, res) => {
   }
 };
 
+// for log in
 const findUserByEmail = async (req, res) => {
-  const result = await authFlowModel.findUserByEmail();
+  try {
+    const { email } = req.params;
+    const userEmail = await authFlowModel.findUserByEmail(email);
+
+    if (!userEmail) {
+      return res
+        .status(404)
+        .json({ message: "[GET /controller]: User email not found..." });
+    }
+
+    const { password_hash: _, ...safeUser } = userEmail;
+
+    res.status(200).json({
+      message: "[GET /controller]: User email found!",
+      data: safeUser,
+    });
+  } catch (err) {
+    console.error(
+      "[GET /controller]: Error finding user by email!",
+      err.message
+    );
+    res.status(500).json({ error: "[GET /controller]: Server error!" });
+  }
+};
+
+// for profile
+const findUserByUsername = async (req, res) => {
+  try {
+    const { username } = req.params;
+
+    const userUsername = await authFlowModel.findUserByUsername(username);
+
+    if (!userUsername) {
+      return res
+        .status(404)
+        .json({ message: "[GET /controller]: User username not found..." });
+    }
+
+    const { password_hash: _, ...safeUser } = userUsername;
+
+    res
+      .status(200)
+      .json({ message: "[GET /controller]: User user found!", data: safeUser });
+  } catch (err) {
+    console.error(
+      "[GET /controller]: Error finding user username!",
+      err.message
+    );
+    res.status(500).json({ error: "[GET /controller]: Server error!" });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const { user_id } = req.params;
+    const { username, first_name, last_name, location } = req.body;
+
+    const updatedUser = await authFlowModel.updateUser(
+      user_id,
+      username,
+      first_name,
+      last_name,
+      location
+    );
+
+    if (!user_id) {
+      return res
+        .status(404)
+        .json({ message: "[PUT /controller]: User not found..." });
+    }
+
+    const { password_hash: _, ...safeUser } = updatedUser;
+
+    res.status(200).json({
+      message: "[PUT /controller]: User updated successfully!",
+      data: safeUser,
+    });
+  } catch (err) {
+    console.error("[PUT /controller]: Error updating user!", err.message);
+    res.status(500).json({ error: "[PUT /controller]: Server Error!" });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const { user_id } = req.params;
+
+    if (!user_id) {
+      return res
+        .status(404)
+        .json({ message: "[DELETE /controller]: User not found!" });
+    }
+
+    const userExist = await authFlowModel.findUserById(user_id);
+
+    if (!userExist) {
+      return res
+        .status(404)
+        .json({ message: "[DELETE /controller]: User not found!" });
+    }
+
+    await authFlowModel.deleteUser(user_id);
+
+    return res
+      .status(200)
+      .json({ message: "[DELETE /controller]: User successfully deleted!" });
+  } catch (err) {
+    console.error("[DELETE /controller]: Error deleting user!", err.message);
+    res.status(500).json("[DELETE /controller]: Server error!");
+  }
+};
+
+module.exports = {
+  createUser,
+  findUserByEmail,
+  findUserByUsername,
+  updateUser,
+  deleteUser,
 };
