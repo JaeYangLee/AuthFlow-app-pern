041ef091ebba3b08014dc619549e1e7c9cb704b2
Jian Lee <ramosjianlee@gmail.com>
2025-10-28 12:22:46 +0800

    feat: done setting up the page templates

---
+++ frontend/src/pages/AFLogInPage.jsx
@@ -0,0 +1,11 @@
+function AFLogInPage() {
+  return (
+    <>
+      <div>
+        <h1>Log In Page</h1>
+      </div>
+    </>
+  );
+}
+
+export default AFLogInPage;
---
+++ frontend/src/pages/AFRegistrationPage.jsx
@@ -0,0 +1,11 @@
+function AFRegistrationPage() {
+  return (
+    <>
+      <div>
+        <h1>Registration Page</h1>
+      </div>
+    </>
+  );
+}
+
+export default AFRegistrationPage;
