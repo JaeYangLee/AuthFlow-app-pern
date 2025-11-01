db9dcae4de4829b576c3594baf10ac403a178d21
Jian Lee <ramosjianlee@gmail.com>
2025-11-01 19:46:53 +0800

    fix: change RESTful call from get to post for the login route

--- backend/routes/authFlowRoutes.js
+++ backend/routes/authFlowRoutes.js
@@ -3,6 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/register", authFlowController.createUser);
-router.get("/login", authFlowController.logInUser);
+router.post("/login", authFlowController.logInUser);
 
 module.exports = router;
