79e29a3dec44848de481fbb1547aeba5fd27c6d8
Jian Lee <ramosjianlee@gmail.com>
2025-11-01 16:19:43 +0800

    feat: finished building the app

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const authFlowRoutes = require("./routes/authFlowRoutes");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use("/users", authFlowRoutes);
+
+module.exports = app;
